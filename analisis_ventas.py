cd20f69c6e9f8050a98713597f663030c81c9134
Ignacio <148559176+iarayae@users.noreply.github.com>
2025-05-21 00:20:18 +0000

    Analisis de ventas cruzadas con soporte 5%

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+
 
 # Leer archivo excel
 archivo = "datos/ventas_cruzadas_ampliado.xlsx"
@@ -25,3 +27,13 @@ df_matrix = pd.DataFrame(transacciones_codificadas, columns=te.columns_)
 # Mostrar las primeras filas de la matriz
 print("Matriz transaccional (boleta vs producto)")
 print(df_matrix.head())
+
+# Generar combinaciones frecuentes con al menos 5% de soporte
+frecuentes = apriori(df_matrix, min_support=0.05, use_colnames=True)
+
+# Filtrar solo los conjuntos con dos o más productos
+frecuentes = frecuentes[frecuentes["itemsets"].apply(lambda x: len(x) >=2)]
+
+# Mostrar los resultados
+print("Conjuntos frecuentes de productos (soporte mínimo 5%): ")
+print(frecuentes.sort_values(by="support", ascending=False))
